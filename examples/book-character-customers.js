d5f38e9835e1504bea59dcda4c020f40df188b29
Nauris Pūķis <hi@pyronaur.com>
2023-05-16 13:03:57 +0300

    Update example 1

--- examples/book-character-customers.js
+++ examples/book-character-customers.js
@@ -5,8 +5,8 @@ const options = {
 	type: "self-healing",
 	openai: {
 		model: "gpt-3.5-turbo",
-		max_tokens: 60,
-		temperature: 0.5,
+		max_tokens: 1000,
+		temperature: 0.991,
 	}
 }
 
@@ -15,12 +15,13 @@ const schema = z.object({
 	customers: z.array(z.object({
 		name: z.string(),
 		email: z.string().email(),
+		lifetime_spend: z.number().positive().describe("Lifetime spend in USD"),
 	})),
 });
 
 async function fetchCustomers() {
 	try {
-		const result = await client.chat("Give me a list of imaginary customers.", schema);
+		const result = await client.chat("10 fictional characters from popular sci-fi books.", schema);
 		console.log(result);
 	} catch (error) {
 		console.error(error);
