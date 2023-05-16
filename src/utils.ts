71474efb767fd08c9ada14075db4bffece9bf875
Nauris Pūķis <hi@pyronaur.com>
2023-05-16 12:05:56 +0300

    Switch back to using JSON7Schema for better accuracy

--- src/utils.ts
+++ src/utils.ts
@@ -3,12 +3,12 @@ import type { z } from 'zod';
 import type { JSONObject } from './types';
 
 export function zod_to_open_api<T extends JSONObject>(schema: z.ZodSchema<T>): JSONObject {
-	return zodToJsonSchema(schema, {
-		target: "openApi3",
+	const json_schema = zodToJsonSchema(schema, {
 		$refStrategy: "none",
 		definitionPath: "schema",
 	}) as JSONObject;
-
+	delete json_schema["$schema"];
+	return json_schema;
 }
 
 export function trim_line_whitespace(content: string): string {
