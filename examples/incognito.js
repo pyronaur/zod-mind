1d38e622c7f20a4398ad5eb5de2f969e94f7a014
Nauris Pūķis <hi@pyronaur.com>
2023-05-31 16:38:06 +0300

    Typo

--- examples/incognito.js
+++ examples/incognito.js
@@ -1,7 +1,5 @@
 import { GPT_Client } from 'zod-mind';
 
-const client = new GPT_Client({
-
-});
+const client = new GPT_Client({});
 const result = await client.incognito_chat("3 fictional characters from popular sci-fi books.");
 console.log(result);
