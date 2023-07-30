369a3dceff87247ca5e0fc496323e74a926f22c0
Nauris Pūķis <hi@pyronaur.com>
2023-07-30 12:08:52 +0300

    Replace plain chat with chat/structured_chat/invoke

--- examples/book-character-customers.ts
+++ examples/book-character-customers.ts
@@ -10,24 +10,25 @@ const fictional_characters = z.object( {
 		name: z.string(),
 		email: z.string().email(),
 		lifetime_spend: z.number().describe( "Lifetime spend in USD" ),
-	} ) ),
+	} ) ).nonempty(),
 } );
 
 const favorite_hats = z.object( {
+	event_name: z.string().describe("Which event are the characters attending?"),
 	customer_hats: z.array(
 		z.object( {
 			name: z.string(),
 			hat: z.string(),
 		} )
 	)
-} );
+} )
 
 async function fetchCustomers() {
 	try {
-		const characters = await client.chat( "3 fictional characters from popular sci-fi books as customers.", fictional_characters );
+		const characters = await client.structured_chat( "3 fictional characters from popular sci-fi books as customers.", fictional_characters );
 		app.info( characters );
 
-		const hats = await client.chat( "What are their favorite hats?", favorite_hats );
+		const hats = await client.structured_chat( "What are their favorite hats?", favorite_hats );
 		app.info( hats );
 	} catch ( error ) {
 		app.error( error );
--- src/Zod_GPT.ts
+++ src/Zod_GPT.ts
@@ -2,9 +2,16 @@ import { trim_line_whitespace, zod_to_open_api } from './utils';
 import { mind } from './logger';
 import { GPT_Client } from "./GPT_Client";
 import { z } from "zod";
-import { AnyValue } from "./types";
 
 const DEFAULT_SYSTEM_MESSAGE = trim_line_whitespace( `You're a helpful AI Assistant` );
+
+type GPT_Function = {
+	name: string,
+	description: string,
+	parameters: z.ZodObject<any>
+}
+
+type Function_Call<F extends GPT_Function[]> = F[number]['name'] | 'auto';
 
 export class Zod_GPT {
 
@@ -12,33 +19,43 @@ export class Zod_GPT {
 		this.client.set_system_message( system_message );
 	};
 
-	/**
-	 * Chat with the GPT and parse the response with Zod.
-	 * @param message - GPT Instructions
-	 * @param zod_schema - Response Format
-	 */
-	public async chat<T extends AnyValue>( message: string, zod_schema: z.ZodSchema<T> ) {
-		mind.debug( "Prompt:", message );
 
-		const structured_response = {
-			name: 'structured_response',
-			description: 'Format your response as a function call',
-			parameters: zod_to_open_api( zod_schema ),
+	public async chat( message: string ) {
+		mind.debug( "Prompt:", message );
+		const result = await this.client.chat( message );
+		mind.debug( "Result:", result );
+		return result;
+	}
+
+	public async invoke<Fn_List extends GPT_Function[], Fn_Name extends Function_Call<Fn_List>>( message: string, functions: Fn_List, function_call: Fn_Name ) {
+
+		const additional_config = {
+			functions: functions.map( fn => {
+				return {
+					name: fn.name,
+					description: fn.description,
+					parameters: zod_to_open_api( fn.parameters ),
+				}
+			} ),
+			function_call: function_call === 'auto' ? ( 'auto' as 'auto' ) : {
+				name: function_call,
+			}
 		}
 
-		const result = await this.client.chat( message, {
-			functions: [structured_response],
-			function_call: {
-				name: 'structured_response',
-			},
-		} );
+		const result = await this.client.chat( message, additional_config );
 
-		mind.debug( "Result:", result );
 		try {
-
 			if ( result.type === "function_call" ) {
-				const json = JSON.parse( result.data.arguments );
-				return zod_schema.parse( json );
+				const func_name = result.data.name as Fn_Name extends 'auto' ? Fn_List[number] : Fn_Name;
+				const func = functions.find( fn => fn.name === func_name );
+				if ( !func ) throw new Error( `Could not find function ${ func_name }` );
+
+				const func_schema = func.parameters;
+				if ( !func_schema ) throw new Error( `Could not find function ${ func_name }` );
+				return {
+					name: func_name,
+					arguments: func_schema.parse( JSON.parse( result.data.arguments ) ),
+				};
 			} else {
 				throw new Error( "Expected a function call response, but got a message." );
 			}
@@ -52,6 +69,30 @@ export class Zod_GPT {
 			} );
 			throw e;
 		}
+	}
+
+	/**
+	 * Chat with the GPT and parse the response with Zod.
+	 * @param message - GPT Instructions
+	 * @param zod_schema - Response Format
+	 */
+	public async structured_chat<T extends z.ZodObject<any>>( message: string, zod_schema: T ): Promise<z.infer<T>> {
+		mind.debug( "Prompt:", message );
+
+		const structured_response = {
+			name: 'structured_response',
+			description: 'Format your response as a function call',
+			parameters: zod_schema,
+		}
+		const result = await this.invoke( message, [ structured_response ], 'structured_response' );
+		mind.debug( "Result:", result );
+		return result.arguments;
+		// const result = await this.client.chat( message, {
+		// 	functions: [ structured_response ],
+		// 	function_call: {
+		// 		name: 'structured_response',
+		// 	},
+		// } );
 
 
 	}
