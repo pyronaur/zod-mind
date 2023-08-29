6c26ba5641947de02cc423df3ef78779e5f9bc7c
Nauris Pūķis <hi@pyronaur.com>
2023-08-29 08:35:46 +0300

    Improve defaults handling

--- index.ts
+++ index.ts
@@ -1,18 +1,37 @@
 import { GPT_Client, GPT_Model, GPT_Request_Config } from './src/GPT_Client';
-import { Zod_GPT } from './src/Zod_GPT';
+import { Zod_GPT, Zod_GPT_Config } from './src/Zod_GPT';
 
 type Zod_Mind_Options = {
 	openai?: GPT_Request_Config,
+	mind?: Partial<Zod_GPT_Config>,
 	api_key?: string,
 }
 
 export function zodMind(config: GPT_Model | Zod_Mind_Options = "gpt-3.5-turbo", key?: string) {
+
+	const gpt_defaults: GPT_Request_Config = {
+		model: "gpt-3.5-turbo",
+	}
 	// If config is a string, assume it's a model name.
-	const options = typeof config === 'string' ? { model: config } : config.openai;
+	const gpt_client_options = typeof config === 'string' ? { model: config } : {
+		...gpt_defaults,
+		...config.openai,
+	};
+
 	// Check for api key in second argument or in config object.
 	const api_key = typeof config === 'string' ? key : config.api_key;
-	const llm = new GPT_Client(options || {}, api_key);
-	return new Zod_GPT(llm);
+	const llm = new GPT_Client(gpt_client_options, api_key);
+
+	const zod_gpt_defaults = {
+		auto_healing: 0,
+		system_message: `You're a helpful AI Assistant`
+	}
+	const zod_gpt_options = typeof config === 'string' ? zod_gpt_defaults : {
+		...zod_gpt_defaults,
+		...config.mind,
+	};
+
+	return new Zod_GPT(llm, zod_gpt_options);
 }
 
 export { GPT_Client } from './src/GPT_Client';
